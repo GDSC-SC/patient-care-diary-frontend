113d7df9f178753a8206bf3a03c939818ec4d41b
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-02-24 04:35:24 +0900

    fix: Home에서 diary 조회 시 없어도 Create하지 않고 Category 받아와서 입력창 그리도록 변경

--- src/utils/manageCategory.ts
+++ src/utils/manageCategory.ts
@@ -13,5 +13,6 @@ export function classifyByCategoryCode(data: any[]) {
     const C003 = data.filter(item => item.categoryCode === 'C003');
     const C004 = data.filter(item => item.categoryCode === 'C004');
     const C005 = data.filter(item => item.categoryCode === 'C005');
-    return [C001, C002, C003, C004, C005];
+    const C006 = data.filter(item => item.categoryCode === 'C006');
+    return [C001, C002, C003, C004, C005, C006];
 }
