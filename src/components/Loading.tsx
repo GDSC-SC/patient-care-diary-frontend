315b3e3189c71daefc1544b8f43817d01fd7133a
0dimen <first11@konkuk.ac.kr>
2024-02-25 15:35:36 +0900

    loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,18 @@
 import '../styles/components/Loading.css'
+import {MutatingDots} from 'react-loader-spinner'
 
 export function Loading() {
-    return <div className="loading">Loading...</div>;
+    return <div className="loading">
+        <MutatingDots
+          visible={true}
+          height="100"
+          width="100"
+          color="#4fa94d"
+          secondaryColor="#4fa94d"
+          radius="12.5"
+          ariaLabel="mutating-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          />
+    </div>;
 }
