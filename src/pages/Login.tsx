a2ff9c6579e3785147ee8e774b9b6eaa7c498770
0dimen <first11@konkuk.ac.kr>
2024-02-25 18:19:46 +0900

    Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
+import { Hearts } from "react-loader-spinner";
 import { MainLayout } from "../components/layout/MainLayout";
+import '../styles/components/Box.css'
 
 export function Login() {
     const fetchData = () => {
@@ -6,8 +8,40 @@ export function Login() {
     };
     
     return (
-        <MainLayout>
-            <button onClick={fetchData}>Login</button>
-        </MainLayout>
+        <div 
+            style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)', // 수직 및 수평 가운데 정렬을 위한 transform 속성 사용
+            }}
+        >
+            <div className="FlexColumn">
+            <div className="FlexRow">
+                <h1>Patient Care Diary</h1>
+                <Hearts
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="hearts-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                    />
+            </div>
+            <div 
+                onClick={fetchData}
+                style={{
+                    display: 'flex',
+                    borderRadius: '30vw',
+                    backgroundColor: '#E5E5E5',
+                    padding: '2vh 10vw',
+                    justifyContent:'center'
+                    }}>
+                    <p style={{ fontSize: '20px' }}>Login</p>
+                </div>
+            </div>
+            
+        </div>
     );
 }
