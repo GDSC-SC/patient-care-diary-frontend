5fa508cac6534da05160c903c96b2d6087cf0c6d
0dimen <first11@konkuk.ac.kr>
2024-02-25 14:35:46 +0900

    footer icon 수정

--- src/components/layout/BottomFooter.tsx
+++ src/components/layout/BottomFooter.tsx
@@ -6,10 +6,10 @@ import { useNavigate } from "react-router-dom";
 const BottomFooter = () => {
     const navigator = useNavigate();
     return(
-        <Footer className="Footer">
-            <Button icon={<Archive/>} onClick={() => {navigator('/feed')}}></Button>
-            <Button icon={<HomeRounded/>} onClick={() => {navigator('/')}}></Button>
-            <Button icon={<FaRegUser/>} onClick={() => {navigator('/myPage')}}></Button>
+        <Footer className="Footer"> 
+            <Button icon={<Archive color="#666666" style={{width:'5vw', height:'5vw'}}/>} onClick={() => {navigator('/feed')}}></Button>
+            <Button icon={<HomeRounded color="#666666" style={{width:'5vw', height:'5vw'}}/>} onClick={() => {navigator('/')}}></Button>
+            <Button icon={<FaRegUser color="#666666" style={{width:'5vw', height:'5vw'}}/>} onClick={() => {navigator('/myPage')}}></Button>
         </Footer>
     )
 }
